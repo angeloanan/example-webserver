58da9a77611a7baefa3a5c654e2cc1eadb2cb59c
Christopher Angelo <angelo@angeloanan.xyz>
2020-06-26 17:34:39 +0700

    feat: the whole thing
    Signed-off-by: Christopher Angelo <angelo@angeloanan.xyz>

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const fastify = require('fastify')({ logger: true })
+
+fastify.get('/', (req, res) => {
+  res.status(200)
+  res.send('I am alive!')
+})
+
+fastify.get('/:key/:value', (req, res) => {
+  const returnBody = {}
+  returnBody[req.params.key] = req.params.value
+
+  res.status(200)
+  res.send(returnBody)
+})
+
+// Run the server!
+fastify.listen(3000, (err) => {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+  fastify.log.info(`Server listening on ${fastify.server.address().port}`)
+})
